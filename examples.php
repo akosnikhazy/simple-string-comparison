2f32f65a80000f149707b4d1ea3707e99383bf21
Ákos Nikházy <604311+akosnikhazy@users.noreply.github.com>
2025-01-06 09:19:04 +0100

    Update examples.php

--- examples.php
+++ examples.php
@@ -6,21 +6,21 @@ spl_autoload_register(function ($class) {
 /* examples */
 	/* string mode off for letter by letter comparison */
 // int(0) the same text
-var_dump(stringDifference::hammeringDistance('The quick brown fox jumps over the lazy dog','The quick brown fox jumps over the lazy dog'));
+var_dump(stringDifference::hammingDistance('The quick brown fox jumps over the lazy dog','The quick brown fox jumps over the lazy dog'));
 
 // int(1) one letter difference
-var_dump(stringDifference::hammeringDistance('The quick brown fex jumps over the lazy dog','The quick brown fox jumps over the lazy dog'));
+var_dump(stringDifference::hammingDistance('The quick brown fex jumps over the lazy dog','The quick brown fox jumps over the lazy dog'));
 
 // int(1) there is a space at the end of the first string
-var_dump(stringDifference::hammeringDistance('The quick brown fox jumps over the lazy dog ','The quick brown fox jumps over the lazy dog'));
+var_dump(stringDifference::hammingDistance('The quick brown fox jumps over the lazy dog ','The quick brown fox jumps over the lazy dog'));
 
 
 	/* bin mode on for bit by bit comparison */
 // int(0) the same text
-var_dump(stringDifference::hammeringDistance('The quick brown fox jumps over the lazy dog','The quick brown fox jumps over the lazy dog',true));
+var_dump(stringDifference::hammingDistance('The quick brown fox jumps over the lazy dog','The quick brown fox jumps over the lazy dog',true));
 
 // int(2) one letter difference
-var_dump(stringDifference::hammeringDistance('The quick brown fex jumps over the lazy dog','The quick brown fox jumps over the lazy dog',true));
+var_dump(stringDifference::hammingDistance('The quick brown fex jumps over the lazy dog','The quick brown fox jumps over the lazy dog',true));
 
 // int(8) there is a space at the end of the first string
-var_dump(stringDifference::hammeringDistance('The quick brown fox jumps over the lazy dog ','The quick brown fox jumps over the lazy dog',true));
+var_dump(stringDifference::hammingDistance('The quick brown fox jumps over the lazy dog ','The quick brown fox jumps over the lazy dog',true));
